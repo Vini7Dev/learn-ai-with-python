48ef14fa41cdf40de6d38e01530b90e08f13277e
Vinícius Gabriel <viniciusgabriel.pb@gmail.com>
2024-12-20 08:38:05 -0300

    feat: :art: add side bar and columns

--- 03-streamlit-web-apps/spotify_project/__init__.py
+++ 03-streamlit-web-apps/spotify_project/__init__.py
@@ -12,8 +12,6 @@ st.set_page_config(
 
 csv_data = pd.read_csv('data/01 Spotify.csv')
 
-csv_data
-
 # st.write('Hello World!')
 # st.write(csv_data[csv_data['Stream'] > 1000000000])
 
@@ -22,14 +20,20 @@ csv_data.set_index('Track', inplace=True)
 # st.line_chart(csv_data[csv_data['Stream'] > 1000000000]['Stream'])
 
 artists = csv_data['Artist'].value_counts().index
-artist = st.selectbox("Artista", artists)
+artist = st.sidebar.selectbox("Artista", artists)
 artist_results = csv_data[csv_data['Artist'] == artist]
 
 albuns = artist_results['Album'].value_counts().index
-algum = st.selectbox("Álbum", albuns)
+algum = st.sidebar.selectbox("Álbum", albuns)
 album_results = csv_data[csv_data['Album'] == algum]
 
 is_hide = st.checkbox('Esconder gráfico')
 
 if not is_hide:
-    st.bar_chart(album_results['Stream'])
+    col1, col2 = st.columns([0.7, 0.3])
+
+    col1.bar_chart(album_results['Stream'])
+
+    col2.line_chart(album_results['Danceability'])
+
+csv_data
