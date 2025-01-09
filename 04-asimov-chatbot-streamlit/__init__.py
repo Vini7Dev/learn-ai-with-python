2e6a668d01e51c53082f52be6cddab3d733d34b2
Vinícius Gabriel <viniciusgabriel.pb@gmail.com>
2025-01-09 08:09:53 -0300

    feat: :art: save and retrieve chats

--- 04-asimov-chatbot-streamlit/__init__.py
+++ 04-asimov-chatbot-streamlit/__init__.py
@@ -8,6 +8,7 @@ from unidecode import unidecode
 
 MESSAGES_FOLDER = Path(__file__).parent / 'messages'
 MESSAGES_FOLDER.mkdir(exist_ok=True)
+CACHE_MESSAGE_NAMES = {}
 
 openai_key = 'API_KEY_HERE'
 
@@ -24,7 +25,16 @@ def get_response_stream(messages, api_key, model='gpt-3.5-turbo', temperature=0)
 def convert_file_name(file_name):
     return re.sub('\W+', '', unidecode(file_name)).lower()
 
+def unconvert_file_name(file_name):
+    if file_name not in CACHE_MESSAGE_NAMES:
+        message_name = load_messages_name_by_file(file_name, key='message_name')
+        CACHE_MESSAGE_NAMES[file_name] = message_name
+        return message_name
+    return CACHE_MESSAGE_NAMES[file_name]
+
 def get_message_name(messages):
+    print('===> messages', messages)
+
     for message in messages:
         if (message['role'] == 'user'):
             return message['content'][:30]
@@ -43,9 +53,9 @@ def save_messages(messages):
     with open(MESSAGES_FOLDER / file_name, 'wb') as f:
         pickle.dump(file_to_save, f)
 
-def load_messages(messages, key='message'):
+def load_messages(messages, key='messages'):
     if len(messages) == 0:
-        return False
+        return []
 
     messaage_name = get_message_name(messages=messages)
     file_name = convert_file_name(messaage_name)
@@ -53,10 +63,18 @@ def load_messages(messages, key='message'):
         messages = pickle.load(f)
     return messages[key]
 
-def main_page():
+def load_messages_name_by_file(file_name, key='messages'):
+    with open(MESSAGES_FOLDER / file_name, 'rb') as f:
+        messages = pickle.load(f)
+    return messages[key]
+
+def inicialization():
     if not 'messages' in st.session_state:
         st.session_state.messages = []
+    if not 'current_chat' in st.session_state:
+        st.session_state.current_chat = ''
 
+def main_page():
     messages = load_messages(st.session_state.messages)
 
     st.header('Asimov Chatbot', divider=True)
@@ -88,4 +106,41 @@ def main_page():
     st.session_state.messages = messages
     save_messages(messages=messages)
 
-main_page()
+def select_chat(file_name):
+    if file_name == '':
+        st.session_state.messages = []
+    else:
+        messages = load_messages_name_by_file(file_name)
+        st.session_state.messages = messages
+    st.session_state.current_chat = file_name
+
+def list_chat_file_names():
+    chats = list(MESSAGES_FOLDER.glob('*'))
+    chats = sorted(chats, key=lambda item: item.stat().st_mtime_ns, reverse=True)
+    return [c.stem for c in chats]
+
+def tab_chats(tab):
+    tab.button('+ Nova conversa',
+               on_click=select_chat,
+               args=('', ),
+               use_container_width=True)
+    tab.markdown('')
+    chat_file_names = list_chat_file_names()
+    if len(chat_file_names) >= 30:
+        chat_file_names += '...'
+    for file_name in chat_file_names:
+        message_name = unconvert_file_name(file_name).capitalize()
+        tab.button(unconvert_file_name(file_name).capitalize(),
+                on_click=select_chat,
+                args=(file_name, ),
+                disabled=file_name==st.session_state.current_chat,
+                use_container_width=True)
+
+def main():
+    inicialization()
+    main_page()
+    tab1, tab2 = st.sidebar.tabs(['Conversas', 'Configurações'])
+    tab_chats(tab1)
+
+if __name__ == '__main__':
+    main()
